cb36731b7b4bf0fca72a001d547c1f4ee148841d
Youjie Zheng <zyj578335934@gmail.com>
2024-09-15 14:04:57 +0845

    Update 2024A

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -18,11 +18,11 @@ fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     let mut ret;
     unsafe {
         asm!(
-            "li x16, 0", // for sbi call id args need 2 reg (x16, x17)
             "ecall",     // sbi call
             inlateout("x10") arg0 => ret, // sbi call arg0 and return value
             in("x11") arg1, // sbi call arg1
             in("x12") arg2, // sbi call arg2
+            in("x16") 0, // for sbi call id args need 2 reg (x16, x17)
             in("x17") which,// sbi call id
         );
     }
